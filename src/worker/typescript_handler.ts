16ddbbc6d1f0a46f87b9f85365c3ec686bacdf6b
Zacarías F. Ojeda <zojeda@gmail.com>
2016-03-05 18:59:45 -0300

    adding functionalities to handler

--- src/worker/typescript_handler.ts
+++ src/worker/typescript_handler.ts
@@ -3,9 +3,6 @@ var workerUtil = require("plugins/c9.ide.language/worker_util");
 var handler = Object.create(PluginBase);
 export = handler;
 
-
-
-// var engineio = require("engine.io");
 handler.handlesLanguage = function(language) {
   return language === "typescript";
 };
@@ -34,21 +31,20 @@ handler.analyze = function(doc, ast, options, callback) {
   tsservice.open(path, doc);
   tsservice.geterr([path], 1)
     .subscribe(diagnostics => {
-        var markers = [];
-        diagnostics.forEach(diag => {
-          markers.push({
-            pos: {
-              sl: diag.start.line - 1,
-              sc: diag.start.offset - 1,
-              el: diag.end.line - 1,
-              ec: diag.end.offset - 1
-            },
-            message: diag.text,
-            level: "error"
-          });
+      var markers = [];
+      diagnostics.forEach(diag => {
+        markers.push({
+          pos: {
+            sl: diag.start.line - 1,
+            sc: diag.start.offset - 1,
+            el: diag.end.line - 1,
+            ec: diag.end.offset - 1
+          },
+          message: diag.text,
+          level: "error"
         });
-        console.log(markers);
-        callback(null, markers);
+      });
+      callback(null, markers);
     },
     error => {
       console.error(error);
@@ -56,55 +52,60 @@ handler.analyze = function(doc, ast, options, callback) {
     });
 };
 
-
 handler.complete = function(doc, ast, pos, options, callback) {
-  console.log("-------------------------");
-  var line = doc.getLine(pos.row);
-  var identifier = options.identifierPrefix;
-  callback(null, [
-    {
-      name: "foo()",
-      replaceText: "foo(^^)",
-      icon: "method",
-      meta: "FooClass",
-      doc: "The foo() method",
-      docHead: "FooClass.foo",
-      priority: 1
-    }
-  ]);
+  let file = this.workspaceDir + options.path;
+  let line = pos.row + 1;
+  let offset = pos.column + 1;
+
+  tsservice.open(file, doc.getValue());
+  tsservice.completions(file, line, offset)
+    .then(completions => {
+      let allCompletions = [];
+      let completionNames = completions.map(comp=> comp.name);
+      tsservice.completionEntryDetails(file, line, offset, completionNames)
+        .then(completionsDetails  => {
+          completionsDetails.forEach(completion => {
+            allCompletions.push({
+              name: completion.name,
+              replaceText: completion.name + (completion.kind === "method" ? "(^^)" : ""),
+              icon: completion.kind + (completion.kindModifiers === "private" ? "2" : ""),
+              doc: completion.documentation.map(dp => dp.text).join(""),
+              docHead: completion.displayParts.map(dp => dp.text).join(""),
+              meta: completion.displayParts.map(dp => dp.text).join(""),
+              isContextual: true,
+              guessTooltip: getCompletionTooltips(file, line, offset, completion),
+              priority: 1
+            });
+          });
+          callback(null, allCompletions);
+        });
+    });
+};
+
+function getCompletionTooltips(file, line, offset, completion: ts.server.protocol.CompletionEntryDetails) {
+  let guessTooltip = completion.name;
+  if(completion.kind=="method") {
+    tsservice.signatureHelp(file, line, offset+completion.name.length+2).then( signatureHelperItems => {
+      guessTooltip = signatureHelperItems.items.map((item) => item.parameters.map(param=>param.displayParts.map(dp=>dp.text)).join("")).join(" ");
+      console.log(guessTooltip);
+    });
+  }
+}
+
+
+handler.jumpToDefinition = function(doc, ast, pos, options, callback) {
+  let file = this.workspaceDir + options.path;
+  let line = pos.row + 1;
+  let offset = pos.column + 1;
+  tsservice.open(file, doc.getValue());
+  tsservice.definition(file, line, offset).then((spans) => {
+    let definition = spans[0];
+    callback(null, {
+      row: definition.start.line - 1,
+      column: definition.start.offset - 1,
+      path: definition.file
+    });
+  });
 };
 
 
-
-
-  // handler.analyzeCurrent = function(path, doc, ast, options, callback) {
-  //   // console.log(Object.keys(doc));
-  //   if (options.isSave) {
-  //     tsservice.open(path);
-  //   } else {
-  //     tsservice.open(path, doc);
-  //   }
-  //   tsservice.geterr([path], 1)
-  //     .subscribe(diagnostics => {
-  //       setTimeout(() => {
-  //         let markers = [];
-  //         diagnostics.forEach(diag => {
-  //           markers.push({
-  //             pos: {
-  //               sl: diag.start.line - 1,
-  //               sc: diag.start.offset - 1,
-  //               el: diag.end.line - 1,
-  //               ec: diag.end.offset - 1
-  //             },
-  //             message: diag.text,
-  //             level: "error"
-  //           });
-  //         });
-  //         callback(null, {
-  //           name: "foo()",
-  //           replaceText: "foo",
-  //           icon: "method",
-  //         }, markers);
-  //       }, 20);
-  //     });
-  // };
