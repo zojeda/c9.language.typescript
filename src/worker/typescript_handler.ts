8cb5e85670c302376f44c319596a28549a541742
Zacarías F. Ojeda <zojeda@gmail.com>
2016-03-06 01:41:17 -0300

    fixing issues with typescript 1.7 and newer

--- src/worker/typescript_handler.ts
+++ src/worker/typescript_handler.ts
@@ -26,32 +26,44 @@ handler.init = function(options) {
 
 };
 
+let MAX_CALL_INTERVAL = 2000;
 handler.analyze = function(doc, ast, options, callback) {
   let path = this.workspaceDir + options.path;
-  tsservice.open(path, doc);
-  tsservice.geterr([path], 1)
-    .subscribe(diagnostics => {
-      var markers = [];
-      diagnostics.forEach(diag => {
-        markers.push({
-          pos: {
-            sl: diag.start.line - 1,
-            sc: diag.start.offset - 1,
-            el: diag.end.line - 1,
-            ec: diag.end.offset - 1
-          },
-          message: diag.text,
-          level: "error"
+  let nowTime = new Date().getTime();
+  if (!this.lastAnalysisTriggered || (nowTime - this.lastAnalysisTriggered) > MAX_CALL_INTERVAL) {
+    tsservice.open(path, doc);
+    this.lastAnalysisTriggered = nowTime;
+    tsservice.geterr([path], 1)
+      .subscribe(diagnostics => {
+        var markers = [];
+        diagnostics.forEach(diag => {
+          markers.push({
+            pos: {
+              sl: diag.start.line - 1,
+              sc: diag.start.offset - 1,
+              el: diag.end.line - 1,
+              ec: diag.end.offset - 1
+            },
+            message: diag.text,
+            level: "error"
+          });
         });
+        callback(null, markers);
+      },
+      error => {
+        console.error(error);
+        callback(error);
       });
-      callback(null, markers);
-    },
-    error => {
-      console.error(error);
-      callback(error);
-    });
+  } else {
+    callback();
+  }
+
 };
 
+//{"command":"open","type":"request","seq":82,"arguments":{"file":"/home/zaca/Development/c9-ws/sample/app.ts"}}
+//{"command":"open","type":"request","seq":82,"arguments":{"file":"/home/zaca/Development/c9-ws/sample/Colab.ts"}}
+//{"command":"completions","type":"request","seq":78,"arguments":{"file":"/home/zaca/Development/c9-ws/sample/app.ts","line":14,"offset":23}}
+//{"command":"completionEntryDetails","type":"request","seq":78,"arguments":{"file":"/home/zaca/Development/c9-ws/sample/app.ts","line":14,"offset":23,"entryNames":["someMethod"]}}
 handler.complete = function(doc, ast, pos, options, callback) {
   let file = this.workspaceDir + options.path;
   let line = pos.row + 1;
@@ -59,37 +71,54 @@ handler.complete = function(doc, ast, pos, options, callback) {
 
   tsservice.open(file, doc.getValue());
   tsservice.completions(file, line, offset)
-    .then(completions => {
+    .subscribe(completions => {
       let allCompletions = [];
-      let completionNames = completions.map(comp=> comp.name);
-      tsservice.completionEntryDetails(file, line, offset, completionNames)
-        .then(completionsDetails  => {
-          completionsDetails.forEach(completion => {
-            allCompletions.push({
+      let consideredCompletions = completions.slice(0, 15);
+      consideredCompletions.forEach(completion => {
+        allCompletions.push({
+          name: completion.name,
+          replaceText: completion.name + (completion.kind === "method" ? "(^^)" : ""),
+          icon: completion.kind + (completion.kindModifiers === "private" ? "2" : ""),
+          isContextual: true,
+          priority: 1
+        });
+      });
+      tsservice.completionEntryDetails(file, line, offset, consideredCompletions.map(c=> c.name))
+        .subscribe(completions => {
+          let index = 0;
+          completions.forEach(completion => {
+            allCompletions[index] = {
               name: completion.name,
               replaceText: completion.name + (completion.kind === "method" ? "(^^)" : ""),
               icon: completion.kind + (completion.kindModifiers === "private" ? "2" : ""),
-              doc: completion.documentation.map(dp => dp.text).join(""),
+              doc: completion.documentation && completion.documentation.map(dp => dp.text).join(""),
               docHead: completion.displayParts.map(dp => dp.text).join(""),
-              meta: completion.displayParts.map(dp => dp.text).join(""),
+              meta: completion.name,
               isContextual: true,
               guessTooltip: getCompletionTooltips(file, line, offset, completion),
               priority: 1
-            });
+            };
+            index++;
           });
-          callback(null, allCompletions);
-        });
-    });
+        }),
+      callback(null, allCompletions)
+    },
+    (error) => callback(error)
+    );
 };
+
+//
 
 function getCompletionTooltips(file, line, offset, completion: ts.server.protocol.CompletionEntryDetails) {
   let guessTooltip = completion.name;
-  if(completion.kind=="method") {
-    tsservice.signatureHelp(file, line, offset+completion.name.length+2).then( signatureHelperItems => {
-      guessTooltip = signatureHelperItems.items.map((item) => item.parameters.map(param=>param.displayParts.map(dp=>dp.text)).join("")).join(" ");
-      console.log(guessTooltip);
-    });
+  if (completion.kind === "method") {
+    tsservice.signatureHelp(file, line, offset + completion.name.length + 2)
+      .subscribe(signatureHelperItems => {
+        guessTooltip = signatureHelperItems.items.map((item) => item.parameters.map(param=> param.displayParts.map(dp=> dp.text)).join("")).join(" ");
+        console.log(guessTooltip);
+      }, err => console.debug(err));
   }
+  return guessTooltip;
 }
 
 
@@ -98,14 +127,17 @@ handler.jumpToDefinition = function(doc, ast, pos, options, callback) {
   let line = pos.row + 1;
   let offset = pos.column + 1;
   tsservice.open(file, doc.getValue());
-  tsservice.definition(file, line, offset).then((spans) => {
-    let definition = spans[0];
-    callback(null, {
-      row: definition.start.line - 1,
-      column: definition.start.offset - 1,
-      path: definition.file
-    });
-  });
+  tsservice.definition(file, line, offset)
+    .subscribe((spans) => {
+      let definition = spans[0];
+      callback(null, {
+        row: definition.start.line - 1,
+        column: definition.start.offset - 1,
+        path: definition.file
+      });
+    },
+    (error) => console.error(error)
+    );
 };
 
 
